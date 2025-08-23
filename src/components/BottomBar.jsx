a3b608d90c1c1ba1c360bf0d60bb0a963c2a1dcd
anshu0908 <eklovyagauba94@gmail.com>
2025-08-23 16:31:17 +0530

    changes

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const BottomBar = () => {
   return (
 <div className='flex items-center justify-center w-full z-50'>
-        <div className='fixed bottom-5 bg-black px-12 rounded-4xl p-4 z-50'>
+        <div className='fixed bottom-5 bg-black px-8 rounded-4xl p-4 z-50'>
                 <div className="text-gray-100 text-xl flex gap-10 ">
                     <div className="hover:text-black cursor-pointer">
                     <Search strokeWidth={0.75} />
